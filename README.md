27eb43fb28c78aff34f29a614bb13a1de96498e0
moeti <moetichentuf@hotmail.com>
2020-05-12 12:47:10 +0200

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# News APP
-
-view here:https://moetichentuf.github.io/news-app/
-
-This application uses the New York Times API.
-For now you can only see the first headline, but the goal is to create my own news app.
---
+++ precache-manifest.a9c39be0bf06ebe4b84f67ae19ae2320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9802e73508bc83a2c8d0ab5e761288",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "43b76f7365d41a119a48",
+    "url": "/news-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "801c6b0ce71464987090",
+    "url": "/news-app/static/js/2.2429e3c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-app/static/js/2.2429e3c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b76f7365d41a119a48",
+    "url": "/news-app/static/js/main.c15730ce.chunk.js"
+  },
+  {
+    "revision": "be6f8f4a5d4c82ca6ae5",
+    "url": "/news-app/static/js/runtime-main.d5952345.js"
+  }
+]);
